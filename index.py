ae09ae3773d644d9d7bc6f7a8ea296da2abcb715
alfianazizi <alfianazizi4869@gmail.com>
2018-04-05 23:14:55 +0700

    Add files via upload
    perubahan capability

--- index.py
+++ index.py
@@ -10,21 +10,63 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.secret_key = 'development key'
 
-def PilihCaps(r,w,x):
+def PilihCapsMon(r,w,x):
     if r == True and w ==True and x == True:
-        return "allow rwx"
+        return "mon", "allow rwx"
     elif r == True and w == True and x == False:
-        return "allow rw"
+        return "mon", "allow rw"
     elif r == True and w == False and x == True:
-        return "allow rx"
+        return "mon", "allow rx"
+    elif r == True and w == False and x == False:
+        return "mon", "allow r"
     elif r == False and w == True and x == True:
-        return "allow wx"
+        return "mon", "allow wx"
     elif r == False and w == False and x == True:
-        return "allow x"
+        return "mon", "allow x"
     elif r == False and w == True and x == False:
-        return "allow w"
+        return "mon", "allow w"
     elif r == False and w == False and x == False:
-        return " "
+        return None
+
+def PilihCapsOsd(r,w,x):
+    if r == True and w ==True and x == True:
+        return "osd", "allow rwx"
+    elif r == True and w == True and x == False:
+        return "osd", "allow rw"
+    elif r == True and w == False and x == True:
+        return "osd", "allow rx"
+    elif r == True and w == False and x == False:
+        return "osd", "allow r"
+    elif r == False and w == True and x == True:
+        return "osd", "allow wx"
+    elif r == False and w == False and x == True:
+        return "osd", "allow x"
+    elif r == False and w == True and x == False:
+        return "osd", "allow w"
+    elif r == False and w == False and x == False:
+        return None 
+
+def PilihCapsMds(r,w,x):
+    if r == True and w ==True and x == True:
+        return "mds", "allow rwx"
+    elif r == True and w == True and x == False:
+        return "mds", "allow rw"
+    elif r == True and w == False and x == True:
+        return "mds", "allow rx"
+    elif r == True and w == False and x == False:
+        return "mds", "allow r"
+    elif r == False and w == True and x == True:
+        return "mds", "allow wx"
+    elif r == False and w == False and x == True:
+        return "mds", "allow x"
+    elif r == False and w == True and x == False:
+        return "mds", "allow w"
+    elif r == False and w == False and x == False:
+        return None    
+
+def DefEntity(x,y,z):
+    if form.MONr.data ==True or form.MONw.data == True or form.MONx.data==True:
+        data = {}
 
 class BuatUser(Form):
     userID = StringField('userID')
@@ -68,8 +110,10 @@ def adduserform():
             return render_template('form.html', form=form)
         else:
             requests.put('http://10.10.6.1:6969/api/v0.1/auth/get-or-create', 
-                params={"entity":"client."+form.userID.data, "caps":["mon", PilihCaps(form.MONr.data,form.MONw.data,form.MONx.data),
-                "osd", PilihCaps(form.OSDr.data,form.OSDw.data,form.OSDx.data),"mds", PilihCaps(form.MDSr.data,form.MDSw.data,form.MDSx.data)]})
+                params={"entity":"client."+form.userID.data, "caps":[ 
+                PilihCapsMon(form.MONr.data,form.MONw.data,form.MONx.data),
+                PilihCapsOsd(form.OSDr.data,form.OSDw.data,form.OSDx.data),
+                PilihCapsMds(form.MDSr.data,form.MDSw.data,form.MDSx.data)]})
             return render_template('success.html')
     elif request.method == 'GET':
         return render_template('form.html', form=form)
